39542ba4d5b51719288c9d94ddbf5d7629264051
MikolHutnik <mikolhutnik@gmail.com>
2018-07-13 11:42:36 -0400

    Review Model w/Properties

--- TravelReview/Models/Reviews.cs
+++ TravelReview/Models/Reviews.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,10 @@ namespace TravelReview.Models
 {
     public class Reviews
     {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime ReviewDate { get; set; }
     }
 }
